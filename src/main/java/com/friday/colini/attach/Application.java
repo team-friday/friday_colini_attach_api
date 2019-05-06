08f537742a8bab516f2fd5259473c05d4fb88c02
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-06 12:08:29 +0900

    기본 셋팅

---
+++ src/main/java/com/friday/colini/attach/Application.java
@@ -0,0 +1,11 @@
+package com.friday.colini.attach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(final String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/com/friday/colini/attach/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friday.colini.attach;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+	@Test
+	public void contextLoads() { }
+}
