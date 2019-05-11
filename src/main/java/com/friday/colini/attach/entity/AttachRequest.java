d611d7de0545734ce471a5ed48f1afb965adec30
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-11 18:47:22 +0900

    AttachRequest 추가 (red)

--- src/main/java/com/friday/colini/attach/entity/AttachRequest.java
+++ src/main/java/com/friday/colini/attach/entity/AttachRequest.java
@@ -4,6 +4,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
+import org.springframework.lang.NonNull;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
 import java.util.List;
@@ -16,4 +18,12 @@ public class AttachRequest extends CreateAuditChildEntity {
     @Getter
     @OneToMany(mappedBy = "attachRequest", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<AttachFile> attachFiles;
+
+    //
+    //
+    //
+
+    public AttachRequest(@NonNull final List<MultipartFile> files) {
+        // TODO: add files
+    }
 }
