df4b068ebb63cca8a76c16ef0d283cf6928258a1
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-11 19:02:18 +0900

    UploadType 메서드 추가

--- src/main/java/com/friday/colini/attach/type/UploadType.java
+++ src/main/java/com/friday/colini/attach/type/UploadType.java
@@ -1,8 +1,39 @@
 package com.friday.colini.attach.type;
 
+import org.springframework.lang.NonNull;
+import org.springframework.web.multipart.MultipartFile;
+
+// TODO: upload and download file 
 public enum UploadType {
-    Local,
-    S3
+    Local {
+        @Override
+        public @NonNull byte[] download(@NonNull final String filePath) {
+            return new byte[0];
+        }
+
+        @Override
+        public @NonNull String upload(@NonNull final MultipartFile file) {
+            return "";
+        }
+    },
+    S3 {
+        @Override
+        public @NonNull byte[] download(@NonNull final String filePath) {
+            return new byte[0];
+        }
+
+        @Override
+        public @NonNull String upload(@NonNull final MultipartFile file) {
+            return "";
+        }
+    },
 
     ;
+
+    //
+    //
+    //
+
+    public abstract @NonNull byte[] download(@NonNull final String filePath);
+    public abstract @NonNull String upload(@NonNull final MultipartFile file);
 }
