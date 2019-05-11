656eb822d00beb387c9c2ce929047be320172375
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-11 18:19:51 +0900

    랜덤 유틸 추가 (red)

---
+++ src/main/java/com/friday/colini/attach/utils/RandomUtils.java
@@ -0,0 +1,15 @@
+package com.friday.colini.attach.utils;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomUtils {
+    public @NonNull String getSecureString(final int length) {
+        return "";
+    }
+
+    public @NonNull String getTimebaseUUID() {
+        return "";
+    }
+}
