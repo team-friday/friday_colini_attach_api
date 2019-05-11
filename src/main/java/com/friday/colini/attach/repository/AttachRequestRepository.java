a9fa0f4a53f0d574c6591535ac6456b3399b13c0
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-11 18:47:22 +0900

    AttachRequest 추가 (red)

---
+++ src/main/java/com/friday/colini/attach/repository/AttachRequestRepository.java
@@ -0,0 +1,6 @@
+package com.friday.colini.attach.repository;
+
+import com.friday.colini.attach.entity.AttachRequest;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttachRequestRepository extends CrudRepository<AttachRequest, Long> { }
