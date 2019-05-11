1a9691f56c68c314cc8eaabdff53c5d4156c339c
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-11 19:18:42 +0900

    AttachRequest 추가 (green)

--- src/main/java/com/friday/colini/attach/entity/AttachFile.java
+++ src/main/java/com/friday/colini/attach/entity/AttachFile.java
@@ -1,13 +1,19 @@
 package com.friday.colini.attach.entity;
 
+import com.friday.colini.attach.properties.FileProperties;
 import com.friday.colini.attach.type.UploadType;
+import com.friday.colini.attach.utils.BeanUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE) // Using JPA
 @Table
@@ -35,4 +41,50 @@ public class AttachFile extends AutoPrimaryEntity {
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @PrimaryKeyJoinColumn(name = "id", referencedColumnName = "id")
     private AttachDownload attachDownload;
+
+    //
+    //
+    //
+
+    AttachFile(@NonNull final MultipartFile file) {
+        originalName = file.getOriginalFilename();
+        contentType = file.getContentType();
+        uploadType = BeanUtils.getBean(FileProperties.class).getUploadType();
+
+        // TODO: upload
+        uploadPath = uploadType.upload(file);
+    }
+
+    @PostPersist
+    private void onPostPersist() {
+        initAttachDownload();
+    }
+
+    //
+    //
+    //
+
+    public long getDownloadCount() {
+        return attachDownload.getCount();
+    }
+
+    public @Nullable LocalDateTime getLastDownloadAt() {
+        return attachDownload.getUpdatedAt().orElse(null);
+    }
+
+    public @Nullable LocalDateTime getCreatedAt() {
+        return attachRequest.getCreatedAt().orElse(null);
+    }
+
+    public boolean isNotEqualsAttachRequestId(long attachRequestId) {
+        return !attachRequest.isEqualsId(attachRequestId);
+    }
+
+    //
+    //
+    //
+
+    private void initAttachDownload() {
+        attachDownload = new AttachDownload(getId());
+    }
 }
