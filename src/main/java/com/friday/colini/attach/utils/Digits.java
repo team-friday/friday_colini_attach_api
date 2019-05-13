9f988c9d7c120b6380903b7dac32943b29f83b6a
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-13 21:33:43 +0900

    Digits를 Digit으로 더 작게 분리!

--- src/main/java/com/friday/colini/attach/utils/Digits.java
+++ src/main/java/com/friday/colini/attach/utils/Digits.java
@@ -4,8 +4,26 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @Component
 class Digits {
+    // TODO: using??
+    private final Digit[] value = new Digit[10];
+
     @Getter(AccessLevel.PACKAGE)
-    private final String value = "0123456789";
+    private final String stringDigits;
+
+    //
+    //
+    //
+
+    Digits() {
+        for (byte i = 0; i < 10; i++) {
+            value[i] = new Digit(i);
+        }
+
+        stringDigits = Arrays.stream(value).map(Digit::toString).collect(Collectors.joining());
+    }
 }
