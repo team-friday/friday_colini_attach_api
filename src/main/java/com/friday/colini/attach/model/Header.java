f48c7dea4838c2ee28135cfd501debdc7eb06be7
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 11:37:17 +0900

    누락된 파일 추가

--- src/main/java/com/friday/colini/attach/model/Header.java
+++ src/main/java/com/friday/colini/attach/model/Header.java
@@ -3,4 +3,15 @@ package com.friday.colini.attach.model;
 import org.springframework.http.HttpHeaders;
 
 public class Header extends HttpHeaders {
+    public void setContentType(final String contentType) {
+        set(Header.CONTENT_TYPE, contentType);
+    }
+
+    public void setContentDisposition(final String contentDisposition) {
+        set(Header.CONTENT_DISPOSITION, contentDisposition);
+    }
+
+    public void setAttachmentFilename(final String filename) {
+        setContentDisposition("attachment; filename=" + filename);
+    }
 }
