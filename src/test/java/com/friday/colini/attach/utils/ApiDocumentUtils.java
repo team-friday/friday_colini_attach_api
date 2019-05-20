88300db06e570cad3e1f33c440ccafa93a6f9f33
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 19:32:21 +0900

    유틸 추가

---
+++ src/test/java/com/friday/colini/attach/utils/ApiDocumentUtils.java
@@ -0,0 +1,19 @@
+package com.friday.colini.attach.utils;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(
+                // modifyUris().scheme("http").host("docs.api.com").removePort(),
+                prettyPrint()
+        );
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
