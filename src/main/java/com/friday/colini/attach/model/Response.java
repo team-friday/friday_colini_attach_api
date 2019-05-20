664d952b30829e6e1a2393484bd16d9a14588c48
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 11:37:17 +0900

    누락된 파일 추가

--- src/main/java/com/friday/colini/attach/model/Response.java
+++ src/main/java/com/friday/colini/attach/model/Response.java
@@ -2,10 +2,9 @@ package com.friday.colini.attach.model;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 
 public class Response<T> extends ResponseEntity<T> {
-    public Response(@NonNull final HttpStatus status) {
+    public Response(final HttpStatus status) {
         super(status);
     }
 }
