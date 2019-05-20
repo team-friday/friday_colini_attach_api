83693fd2b976eff95846ef88bae196a39b2b51df
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 19:29:51 +0900

    rest docs 설정 추가

--- src/test/java/com/friday/colini/attach/controller/ControllerSupport.java
+++ src/test/java/com/friday/colini/attach/controller/ControllerSupport.java
@@ -3,6 +3,7 @@ package com.friday.colini.attach.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 abstract class ControllerSupport {
     @Autowired
     MockMvc mockMvc;
