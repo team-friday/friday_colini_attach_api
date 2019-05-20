66eb24a9625537a699733fc02cfbf5416cbcc7b0
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 14:03:45 +0900

    TestRestTemplate to MockMvc
    * TestRestTemplate는 외부 API 테스트 시 많이 사용된다

--- src/test/java/com/friday/colini/attach/controller/ControllerSupport.java
+++ src/test/java/com/friday/colini/attach/controller/ControllerSupport.java
@@ -1,14 +1,22 @@
 package com.friday.colini.attach.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
 abstract class ControllerSupport {
     @Autowired
-    TestRestTemplate template;
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 }
