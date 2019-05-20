8969d663dd2bcb867cffb66f2c927cfaa01f033b
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-20 19:29:51 +0900

    rest docs 설정 추가

--- src/test/java/com/friday/colini/attach/controller/DefaultControllerTests.java
+++ src/test/java/com/friday/colini/attach/controller/DefaultControllerTests.java
@@ -2,9 +2,15 @@ package com.friday.colini.attach.controller;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 
+import static com.friday.colini.attach.utils.ApiDocumentUtils.getDocumentResponse;
+import static com.friday.colini.attach.utils.ApiDocumentUtils.getDocumentRequest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +21,14 @@ public class DefaultControllerTests extends ControllerSupport {
         final var result = mockMvc.perform(get("/health"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andDo(
+                        document(
+                                "default/health-check",
+                                getDocumentRequest(),
+                                getDocumentResponse(),
+                                pathParameters()
+                        )
+                )
                 .andReturn()
                 ;
 
@@ -28,6 +42,18 @@ public class DefaultControllerTests extends ControllerSupport {
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("errCode").value(Matchers.is("404")))
                 .andExpect(jsonPath("errMessage").value(Matchers.is("NOT_FOUND")))
+                .andDo(
+                        document(
+                                "default/not-found",
+                                getDocumentRequest(),
+                                getDocumentResponse(),
+                                pathParameters(),
+                                responseFields(
+                                        fieldWithPath("errCode").type(JsonFieldType.STRING).description("에러 코드"),
+                                        fieldWithPath("errMessage").type(JsonFieldType.STRING).description("에러 메시지")
+                                )
+                        )
+                )
         ;
     }
 }
