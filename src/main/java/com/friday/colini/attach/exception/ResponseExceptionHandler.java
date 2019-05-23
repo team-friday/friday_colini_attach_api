fe6341a8c2f06faf10a7f90a0d2abc64586fd464
Jaeyeonling <jaeyeonling@gmail.com>
2019-05-23 12:31:01 +0900

    에러 핸들링

---
+++ src/main/java/com/friday/colini/attach/exception/ResponseExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.friday.colini.attach.exception;
+
+import com.amazonaws.AmazonServiceException;
+import com.friday.colini.attach.model.Error;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(PlatformException.class)
+    public ResponseEntity<Error> platformException(final PlatformException e) {
+        return e.toResponse();
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<Error> amazonServiceException(final AmazonServiceException e) {
+        log.error("Amazon service exception", e);
+
+        return PlatformStatus.INTERNAL_SERVER_ERROR.toResponse();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> uncaughtException(final Exception e) {
+        log.error("Uncaught exception", e);
+
+        return PlatformStatus.INTERNAL_SERVER_ERROR.toResponse();
+    }
+}
